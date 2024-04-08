b958e2d9a7235fe6d48d4ed69a7472f51d5313b9
Pedro Wison <pedrowpvh@hotmail.com>
2024-04-08 10:58:18 -0400

    feat: formulario

--- src/app/pages/dashboard/login/Login.tsx
+++ src/app/pages/dashboard/login/Login.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react"
 import { useNavigate} from "react-router-dom"
 
+
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEntrar = () => {
+        console.log(email)
+        console.log(password)
+    }
     return(
         <div>
-            
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+                <button type="button" onClick={handleEntrar}>
+                    entrar
+                </button>
+            </form>
         </div>
     )
 } 
