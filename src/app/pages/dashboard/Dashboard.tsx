398c67fc64bde11cf5434b1459f393d47c6ff742
PedroBritoWR <pedrowpvh@hotmail.com>
2024-04-18 13:34:31 -0400

    trablhando com api

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,22 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
-interface ITarefa {
-    id: number;
-    title: string;
-    isCompleted: boolean;
-}
+import { ITarefa, TarefasService } from '../../services/api/tarefas/TarefasService';
+import { ApiException } from '../../services/api/ApiException';
 
 
 export const Dashboard = () => {
     const [lista, setLista] = useState<ITarefa[]>([]);
+
+    useEffect(() => {
+        TarefasService.getAll()
+            .then((result) => {
+                if (result instanceof ApiException) {
+                    alert(result.message)
+                } else {
+                    setLista(result);
+                }
+            })
+    }, [])
 
     const handleInputKeydown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
         if (e.key === 'Enter') {
